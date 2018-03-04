f1c81cc3323450f32464ee1bfa8b93a6a43f1cb1
Michel-FK <michel.stempin@funkey-project.com>
2018-03-04 19:10:30 +0100

    Added rootfs resize, automatic swap and Wi-Fi activation

--- board/funkey/post-build.sh
+++ board/funkey/post-build.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 BOARD_DIR="$( dirname "${0}" )"
 MKIMAGE="${HOST_DIR}/bin/mkimage"
+MKSWAP="${HOST_DIR}/sbin/mkswap"
 BOOT_CMD="${BOARD_DIR}/boot.cmd"
 BOOT_CMD_H="${BINARIES_DIR}/boot.scr"
 
 # U-Boot script
 "${MKIMAGE}" -C none -A arm -T script -d "${BOOT_CMD}" "${BOOT_CMD_H}"
+
+# Swap
+sed -i '/^\/swap/d' "${TARGET_DIR}/etc/fstab"
+echo "/swap		none		swap	defaults	0	0" >> "${TARGET_DIR}/etc/fstab"
