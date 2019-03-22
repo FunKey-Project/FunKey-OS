8b88601d04b9ba329a7d7450c83a7993f0013b00
Michel-FK <michel.stempin@funkey-project.com>
2019-03-22 13:58:51 +0100

    experimental FunKey DTS

--- board/funkey/boot.cmd
+++ board/funkey/boot.cmd
@@ -1,4 +1,4 @@
 setenv bootargs console=ttyS0,115200 panic=5 console=tty0 rootwait fbcon=map:10 fbcon=font:VGA8x8 root=/dev/mmcblk0p2 earlyprintk rw
 load mmc 0:1 0x41000000 zImage
-load mmc 0:1 0x41800000 sun8i-v3s-licheepi-zero-dock.dtb
+load mmc 0:1 0x41800000 sun8i-v3s-funkey.dtb
 bootz 0x41000000 - 0x41800000
