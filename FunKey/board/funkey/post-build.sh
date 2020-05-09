10150b111da3ebe9b6c3242d39a090a3d4e104b8
Michel-FK <michel.stempin@funkey-project.com>
2020-05-09 23:49:37 +0200

    first_boot script

--- FunKey/board/funkey/post-build.sh
+++ FunKey/board/funkey/post-build.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-# Swap
+# Add swap partition to fstab
 sed -i '/^\/swap/d' "${TARGET_DIR}/etc/fstab"
 echo "/swap		none		swap	defaults	0	0" >> "${TARGET_DIR}/etc/fstab"
+
+# Add local path to init scripts
+sed -i '3iexport PATH=/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/sbin:/usr/local/bin' ${TARGET_DIR}/etc/init.d/rcK
+sed -i '3iexport PATH=/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/sbin:/usr/local/bin' ${TARGET_DIR}/etc/init.d/rcS
+
+# Remove log daemon init scripts since they are loaded from inittab
+rm -f ${TARGET_DIR}/etc/init.d/S01syslogd ${TARGET_DIR}/etc/init.d/S02klogd
