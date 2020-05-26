8ab7ca124d63d6965290ff99d801afaf1d73da6a
Michel-FK <michel.stempin@funkey-project.com>
2020-05-26 10:15:55 +0200

    Added FunKey-Emulator-picodrive

---
+++ FunKey/package/picodrive/picodrive.mk
@@ -0,0 +1,64 @@
+################################################################################
+#
+# picodrive
+#
+################################################################################
+
+PICODRIVE_VERSION = 863b75d0e3191118d7f0527a2320724f2a988fe6
+PICODRIVE_SITE_METHOD = git
+PICODRIVE_SITE = ssh://git@fk/FunKey-Project/FunKey-Emulator-picodrive
+PICODRIVE_LICENSE = MAME
+PICODRIVE_LICENSE_FILES = COPYING
+
+PICODRIVE_DEPENDENCIES = sdl sdl_image sdl_mixer sdl_ttf zlib
+
+PICODRIVE_CFLAGS = $(TARGET_CFLAGS)
+
+ifeq ($(BR2_ARM_CPU_ARMV7A),y)
+PICODRIVE_CFLAGS += -march=armv7-a
+endif
+
+ifeq ($(BR2_GCC_TARGET_CPU),"cortex-a7")
+PICODRIVE_CFLAGS += -mtune=cortex-a7
+endif
+
+ifeq ($(BR2_GCC_TARGET_FLOAT_ABI),"hard")
+PICODRIVE_CFLAGS += -mfloat-abi=hard -ffast-math -funsafe-math-optimizations
+else ifeq ($(BR2_GCC_TARGET_FLOAT_ABI),"soft")
+PICODRIVE_CFLAGS += -mfloat-abi=soft -ffast-math -funsafe-math-optimizations
+endif
+
+ifeq ($(BR2_ARM_CPU_HAS_NEON),y)
+PICODRIVE_CFLAGS += -D__ARM_NEON__ -mfpu=neon -mvectorize-with-neon-quad
+endif
+
+PICODRIVE_CONF_OPTS += --platform=generic --sound-drivers=sdl
+PICODRIVE_CFLAGS += -ggdb -O3
+
+PICODRIVE_LIBS += -lSDL_image -lSDL_ttf
+
+define PICODRIVE_CONFIGURE_CMDS
+	(cd $(@D); \
+	chmod +x configure; \
+	sed -i -e 's/-mcpu/-mtune/g' configure; \
+	CFLAGS='$(PICODRIVE_CFLAGS)' \
+	CROSS_COMPILE=$(TARGET_CROSS) \
+	LDFLAGS='-L$(TARGET_DIR)/usr/lib' \
+	LDLIBS='$(PICODRIVE_LIBS)'\
+	./configure $(PICODRIVE_CONF_OPTS) \
+	)
+endef
+
+define PICODRIVE_BUILD_CMDS
+	(cd $(@D); \
+	make \
+	)
+endef
+
+define PICODRIVE_INSTALL_TARGET_CMDS
+	$(INSTALL) -d -m 0755 $(TARGET_DIR)/usr/games
+	$(INSTALL) -m 0755 $(@D)/PicoDriveBin $(TARGET_DIR)/usr/games/PicoDriveBin
+endef
+
+
+$(eval $(generic-package))
