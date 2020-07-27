e9eeba116555e1b4931eaead0ae64545c36b1ed5
Michel-FK <michel.stempin@funkey-project.com>
2020-07-27 22:38:38 +0200

    Append ramdisk to kernel

--- PCBA/board/funkey/boot.cmd
+++ PCBA/board/funkey/boot.cmd
@@ -1,3 +1,3 @@
 setenv bootargs console=ttyS0,115200
 setenv fdt_high 0xffffffff
-bootm 0x41000000 0x41b00000 0x41800000
+bootm 0x41000000 - 0x41800000
--- fel-boot.sh
+++ fel-boot.sh
@@ -3,6 +3,5 @@ cd PCBA/output/images
 sudo ~/sunxi-tools/sunxi-fel -v -p uboot u-boot-sunxi-with-spl.bin \
      write 0x41000000 uImage \
      write 0x41800000 sun8i-v3s-funkey.dtb \
-     write 0x41900000 boot.scr \
-     write 0x41B00000 rootfs.cpio.uboot
+     write 0x41900000 boot.scr
 cd -
