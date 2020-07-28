48d9638378ef4cbfe6902d44ff18e31476777db4
Michel-FK <michel.stempin@funkey-project.com>
2020-07-28 22:16:22 +0200

    PCBA uses zImage kernel

--- PCBA/board/funkey/boot.cmd
+++ PCBA/board/funkey/boot.cmd
@@ -1,2 +1,2 @@
 setenv bootargs console=ttyS0,115200 panic=5 quiet
-bootm 0x41000000 - 0x41800000
+bootz 0x41000000 - 0x41800000
--- fel-boot.sh
+++ fel-boot.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 cd PCBA/output/images
 sudo ~/sunxi-tools/sunxi-fel -v -p uboot u-boot-sunxi-with-spl.bin \
-     write 0x41000000 uImage \
-     write 0x41800000 sun8i-v3s-funkey.dtb \
-     write 0x41900000 boot.scr
+     multi 3 \
+     0x41000000 zImage \
+     0x41800000 sun8i-v3s-funkey.dtb \
+     0x41900000 boot.scr
 cd -
