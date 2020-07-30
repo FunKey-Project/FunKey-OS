e377ff799b568e347cd6a88100c914da4b361bb3
Michel-FK <michel.stempin@funkey-project.com>
2020-07-30 22:42:14 +0200

    Skimmed busybox

--- PCBA/board/funkey/post-build.sh
+++ PCBA/board/funkey/post-build.sh
@@ -3,5 +3,11 @@
 # Remove log daemon init scripts since they are loaded from inittab
 rm -f ${TARGET_DIR}/etc/init.d/S01syslogd ${TARGET_DIR}/etc/init.d/S02klogd
 
+# Remove ssyctl daemon
+rm -f ${TARGET_DIR}/etc/init.d/S02sysctl
+
+# Remove random daemon
+rm -f ${TARGET_DIR}/etc/init.d/S20urandom
+
 # Remove telnet daemon as we don't have network
 rm -f ${TARGET_DIR}/etc/init.d/S50telnet
