829a1c397363302bc903500993b166cb9cfdb933
Michel Stempin <michel.stempin@wanadoo.fr>
2020-10-13 13:41:02 +0200

    double boot and rootfs partitions
    Signed-off-by: Michel Stempin <michel.stempin@wanadoo.fr>

--- FunKey/board/funkey/boot.cmd
+++ FunKey/board/funkey/boot.cmd
@@ -1,4 +1,4 @@
-setenv bootargs console=ttyS0,115200 panic=5 rootwait fbcon=map:10 fbcon=font:VGA8x8 vt.global_cursor_default=0 root=/dev/mmcblk0p2 rootfstype=ext4 rootflags=commit=120,data=writeback,barrier=0,journal_async_commit rw quiet
+setenv bootargs console=ttyS0,115200 panic=5 rootwait fbcon=map:10 fbcon=font:VGA8x8 vt.global_cursor_default=0 root=/dev/mmcblk0p3 rootfstype=ext4 rootflags=commit=120,data=writeback,barrier=0,journal_async_commit rw quiet
 load mmc 0:1 0x41000000 uImage
 load mmc 0:1 0x41800000 sun8i-v3s-funkey.dtb
 bootm 0x41000000 - 0x41800000
