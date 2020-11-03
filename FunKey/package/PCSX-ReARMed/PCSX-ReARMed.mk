582e3453fc7db06f06a64bf96e26a0ff9acea55c
Vincent-FK <vincent.buso@funkey-project.com>
2020-11-03 23:25:18 +0100

    new PCSX version with absolute paths and memcards in shared partition

--- FunKey/package/PCSX-ReARMed/PCSX-ReARMed.mk
+++ FunKey/package/PCSX-ReARMed/PCSX-ReARMed.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PCSX_REARMED_VERSION = b1a49c8fce13a471d6d57a21eaaa253dc866653e
+PCSX_REARMED_VERSION = 454ecc684bff1bdd989652696ee2f45a0dc45da1
 PCSX_REARMED_SITE_METHOD = git
 PCSX_REARMED_SITE = ssh://git@fk/FunKey-Project/FunKey-Emulator-PCSX-ReARMed
 PCSX_REARMED_LICENSE = GPL-2.0
