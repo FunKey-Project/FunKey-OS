bbddf7004a15b931d3f69f1ac517e14211b22726
Michel-FK <michel.stempin@funkey-project.com>
2020-12-05 15:33:09 +0100

    use our forked repos for gmenu2x
    Signed-off-by: Michel-FK <michel.stempin@funkey-project.com>

--- FunKey/package/fonts-droid/fonts-droid.mk
+++ FunKey/package/fonts-droid/fonts-droid.mk
@@ -8,6 +8,7 @@ FONTS_DROID_VERSION = 074990596701553b8b51ff22290453de522f0d15
 FONTS_DROID_SITE = https://android.googlesource.com/platform/frameworks/base/+archive/$(FONTS_DROID_VERSION)/data
 FONTS_DROID_SOURCE = fonts.tar.gz
 FONTS_DROID_LICENSE = Apache-2.0
+
 FONTS_DROID_STRIP_COMPONENTS = 0
 
 # We cannot verify the hash because googlesource.com produces an archive
