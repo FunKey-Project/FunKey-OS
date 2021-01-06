a0b6d3f8e8a376253681dc6d9c07817c532d0bdf
Vincent-FK <vincent.buso@funkey-project.com>
2021-01-06 02:04:52 +0100

    retrofe new version that stops ampli when necessary, start_launcher stopping/restrarting ampli when necessary

--- FunKey/package/retrofe/retrofe.mk
+++ FunKey/package/retrofe/retrofe.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RETROFE_VERSION = 343f64f
+RETROFE_VERSION = d07ca9c
 RETROFE_SITE_METHOD = git
 RETROFE_SITE = https://github.com/FunKey-Project/RetroFE.git
 RETROFE_DEPENDENCIES = gstreamer1 gst1-plugins-base sdl sdl_image sdl_mixer sdl_sound sdl_ttf libglib2 sqlite zlib
