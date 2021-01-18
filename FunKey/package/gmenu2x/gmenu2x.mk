7f418686bb9521acb73b65a9675ebfceaddf6f52
Vincent-FK <vincent.buso@funkey-project.com>
2021-01-18 23:13:39 +0100

    gmenu2x release 1.0.1

--- FunKey/package/gmenu2x/gmenu2x.mk
+++ FunKey/package/gmenu2x/gmenu2x.mk
@@ -3,7 +3,7 @@
 # gmenu2x
 #
 #############################################################
-GMENU2X_VERSION = gmenu2x-FunKey-1.0.0
+GMENU2X_VERSION = gmenu2x-FunKey-1.0.1
 GMENU2X_SITE_METHOD = git
 GMENU2X_SITE = https://github.com/FunKey-Project/gmenu2x.git
 GMENU2X_LICENSE = GPL-2.0
