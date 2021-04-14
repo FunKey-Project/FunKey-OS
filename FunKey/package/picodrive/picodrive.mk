6b05f7b8e603168bb5e8e9e6bbc9dd084eca1b2c
Michel-FK <michel.stempin@funkey-project.com>
2021-04-14 23:25:58 +0200

    bump FCEUX & picodrive version
    Signed-off-by: Michel-FK <michel.stempin@funkey-project.com>

--- FunKey/package/picodrive/picodrive.mk
+++ FunKey/package/picodrive/picodrive.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PICODRIVE_VERSION = 5a161d92
+PICODRIVE_VERSION = 009d007
 PICODRIVE_SITE_METHOD = git
 PICODRIVE_SITE = https://github.com/FunKey-Project/picodrive-irixxxx.git
 PICODRIVE_LICENSE = MAME
