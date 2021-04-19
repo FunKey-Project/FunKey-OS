fd35689bdf88caadb101ee955ea4f26cebe7119a
Vincent-FK <vincent.buso@funkey-project.com>
2021-04-20 00:08:27 +0200

    bump libopk version

--- FunKey/package/libopk/libopk.mk
+++ FunKey/package/libopk/libopk.mk
@@ -3,8 +3,7 @@
 # libopk
 #
 #############################################################
-#LIBOPK_VERSION = libopk-FunKey-1.0.1
-LIBOPK_VERSION = 3dd25b6
+LIBOPK_VERSION = libopk-FunKey-1.0.2
 LIBOPK_SITE_METHOD = git
 LIBOPK_SITE = https://github.com/FunKey-Project/libopk.git
 
