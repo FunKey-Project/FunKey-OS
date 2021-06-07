8ba4b079643cb01371860927906c0a6ab00cf4fc
Michel-FK <michel.stempin@funkey-project.com>
2021-06-07 22:34:57 +0200

    use new share script
    Signed-off-by: Michel-FK <michel.stempin@funkey-project.com>

--- FunKey/package/gmenu2x/gmenu2x.mk
+++ FunKey/package/gmenu2x/gmenu2x.mk
@@ -3,7 +3,8 @@
 # gmenu2x
 #
 #############################################################
-GMENU2X_VERSION = 737b86e
+
+GMENU2X_VERSION = 9f83250
 GMENU2X_SITE_METHOD = git
 GMENU2X_SITE = https://github.com/FunKey-Project/gmenu2x.git
 GMENU2X_LICENSE = GPL-2.0
